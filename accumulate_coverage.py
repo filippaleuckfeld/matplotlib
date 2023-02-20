1680d5b347f7384f0b8f6dd47dc753ac5f2c90d5
haval0 <56519858+haval0@users.noreply.github.com>
2023-02-20 22:18:35 +0100

    feat: add DIY coverage for boxplot@_axes.py
    Resolves #12

--- accumulate_coverage.py
+++ accumulate_coverage.py
@@ -1,8 +1,8 @@
 def main():
-    with open("coverage_spectral_helper.txt", "r") as f:
+    with open("coverage_boxplot.txt", "r") as f:
         contents = f.readlines()
         lists = [eval(i) for i in contents]
-        out = [any(lists[:][i]) for i in range(len(lists[0]))]
+        out = [any(i) for i in zip(*lists)]
         num = out.count(True)
         print(f"coverage -- {num}/{len(out)}: {num * 100/len(out)}%: {out}")
 
